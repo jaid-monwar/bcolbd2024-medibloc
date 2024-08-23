1ecf775226e78bd281b719cd0994c7800e642fae
Jaid Monwar Chowdhury <jaid.yobo@gmail.com>
2024-08-23 18:05:07 +0600

    chnaged backend structure for personal,diagnosis

--- api/src/controllers/prescription.controller.js
+++ api/src/controllers/prescription.controller.js
@@ -2,17 +2,32 @@ const httpStatus = require("http-status");
 const pick = require("../utils/pick");
 const ApiError = require("../utils/ApiError");
 const catchAsync = require("../utils/catchAsync");
-const { userService, diagnosisService } = require("../services");
+const { userService, prescriptionService } = require("../services");
 const { getPagination } = require("../utils/pagination");
 const { getSuccessResponse } = require("../utils/Response");
 
-const createDiagnosis = catchAsync(async (req, res) => {
+const createPrescription = catchAsync(async (req, res) => {
   let { user } = req.loggerInfo;
-  let fileMetadata = req.body.fileMetadata;
   console.log("============user========", user);
-  const result = await diagnosisService.createDiagnosis(
-    req.body,
-    fileMetadata,
+  const result = await prescriptionService.createPrescription(req.body, user);
+  res
+    .status(httpStatus.CREATED)
+    .send(
+      getSuccessResponse(
+        httpStatus.CREATED,
+        "Prescription form created successfully",
+        result
+      )
+    );
+});
+
+const createPersonalInfo = catchAsync(async (req, res) => {
+  let { user } = req.loggerInfo;
+  let personalinfoData = req.body;
+  let prescriptionId = req.params.id;
+  const result = await prescriptionService.createPersonalInfo(
+    personalinfoData,
+    prescriptionId,
     user
   );
   res
@@ -20,7 +35,27 @@ const createDiagnosis = catchAsync(async (req, res) => {
     .send(
       getSuccessResponse(
         httpStatus.CREATED,
-        "Diagnosis created successfully",
+        "personal info form submitted successfully",
+        result
+      )
+    );
+});
+
+const createDiagnosis = catchAsync(async (req, res) => {
+  let { user } = req.loggerInfo;
+  let diagnosisData = req.body;
+  let prescriptionId = req.params.id;
+  const result = await prescriptionService.createDiagnosis(
+    diagnosisData,
+    prescriptionId,
+    user
+  );
+  res
+    .status(httpStatus.CREATED)
+    .send(
+      getSuccessResponse(
+        httpStatus.CREATED,
+        "diagnosis form submitted successfully",
         result
       )
     );
@@ -58,7 +93,7 @@ const getSignedURL = catchAsync(async (req, res) => {
   );
 });
 
-const getDiagnoses = catchAsync(async (req, res) => {
+const getPrescriptions = catchAsync(async (req, res) => {
   const { pageSize, bookmark, filterType } = req.query;
 
   let { orgId, email } = req.loggerInfo.user;
@@ -75,7 +110,7 @@ const getDiagnoses = catchAsync(async (req, res) => {
 
   console.log(filter);
 
-  let data = await diagnosisService.queryDiagnoses(filter);
+  let data = await prescriptionService.queryPrescriptions(filter);
   if (data?.data) {
     data.data = data.data.map((elm) => elm.Record);
   }
@@ -98,6 +133,56 @@ const getHistoryById = catchAsync(async (req, res) => {
     .send(
       getSuccessResponse(httpStatus.OK, "Agreement fetched successfully", data)
     );
+});
+
+const getPersonalInfosByPrescriptionId = catchAsync(async (req, res) => {
+  const { pageSize, bookmark } = req.query;
+  const prescriptionId = req.params.id;
+  let { orgId, email } = req.loggerInfo.user;
+  let orgName = `org${orgId}`;
+
+  let filter = {
+    orgId: parseInt(req.loggerInfo.user.orgId),
+    pageSize: pageSize || "10",
+    bookmark: bookmark || "",
+    orgName,
+    email,
+    prescriptionId,
+  };
+
+  let data = await prescriptionService.queryPersonalInfosByPrescriptionId(
+    filter
+  );
+  data = data.data.map((elm) => elm.Record);
+  res.status(httpStatus.OK).send(
+    getSuccessResponse(httpStatus.OK, "Users fetched successfully", {
+      personalinfos: data,
+    })
+  );
+});
+
+const getDiagnosesByPrescriptionId = catchAsync(async (req, res) => {
+  const { pageSize, bookmark } = req.query;
+  const prescriptionId = req.params.id;
+  let { orgId, email } = req.loggerInfo.user;
+  let orgName = `org${orgId}`;
+
+  let filter = {
+    orgId: parseInt(req.loggerInfo.user.orgId),
+    pageSize: pageSize || "10",
+    bookmark: bookmark || "",
+    orgName,
+    email,
+    prescriptionId,
+  };
+
+  let data = await prescriptionService.queryDiagnosesByPrescriptionId(filter);
+  data = data.data.map((elm) => elm.Record);
+  res.status(httpStatus.OK).send(
+    getSuccessResponse(httpStatus.OK, "Users fetched successfully", {
+      diagnoses: data,
+    })
+  );
 });
 
 const getApprovalsByAgreementId = catchAsync(async (req, res) => {
@@ -124,16 +209,20 @@ const getApprovalsByAgreementId = catchAsync(async (req, res) => {
   );
 });
 
-const getDiagnosisById = catchAsync(async (req, res) => {
+const getPrescriptionById = catchAsync(async (req, res) => {
   const { id } = req.params;
 
   let { user } = req.loggerInfo;
-  let data = await diagnosisService.queryDiagnosisById(id, user);
+  let data = await prescriptionService.queryPrescriptionById(id, user);
 
   res
     .status(httpStatus.OK)
     .send(
-      getSuccessResponse(httpStatus.OK, "Diagnosis fetched successfully", data)
+      getSuccessResponse(
+        httpStatus.OK,
+        "Prescription form fetched successfully",
+        data
+      )
     );
 });
 
@@ -158,14 +247,18 @@ const deleteUser = catchAsync(async (req, res) => {
 });
 
 module.exports = {
+  createPrescription,
+  getPrescriptions,
+  createPersonalInfo,
   createDiagnosis,
-  getDiagnoses,
   getUser,
   updateUser,
   deleteUser,
-  getDiagnosisById,
+  getPrescriptionById,
   approveAgreement,
   getApprovalsByAgreementId,
+  getPersonalInfosByPrescriptionId,
+  getDiagnosesByPrescriptionId,
   getSignedURL,
   getHistoryById,
 };
